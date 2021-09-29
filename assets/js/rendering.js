1a1753f7e172d9624b27e37c4a26567af37673db
Omar-Ess <souafomar@gmail.com>
2021-09-29 16:21:48 +0100

    finished testimonials section

---
+++ assets/js/rendering.js
@@ -0,0 +1,42 @@
+(function () {
+//   const testimonials = [
+//     {
+//       text: "غزال داكشي عندهم الأكل رائع انا تنحماق على الماكلة ديالهم بنينة بزاف",
+//       person: "Dounia Batma",
+//       profession: "chanteuse",
+//     },
+//     {
+//       text: "toujours un plaisir de savourer les sushis de katsu vraiment c'est trop bon",
+//       person: "sonia ngadi",
+//       profession: "influenceur",
+//     },
+//     {
+//       text: "ç'est très bon surtout les tacos j'ai rarement manger des tacos Bnan bhal haka",
+//       person: "leila hadioui",
+//       profession: "actrice",
+//     },
+//   ];
+//   const swiperWrapper = document.querySelector(".testimonials-slider .swiper-wrapper");
+//   let html = "";
+//   testimonials.forEach((testimonial, index) => {
+//     html += `<div class="swiper-slide">
+//        <div  class="testimonial-item">
+//          <img src="assets/img/testimonials/testimonials-${index + 1}.jpg" class="testimonial-img" alt="${
+//       testimonial.person
+//     }" />
+//          <h3>${testimonial.person}</h3>
+//          <h4>${testimonial.profession}</h4>
+//          <div class="stars">
+//            <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i
+//            ><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+//          </div>
+//          <p>
+//            <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+//            ${testimonial.text}
+//            <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+//          </p>
+//        </div>
+//      </div>`;
+//   });
+//   swiperWrapper.innerHTML = html;
+})();
