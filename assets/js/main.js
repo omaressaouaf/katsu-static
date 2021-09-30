5901872f523f0b47c1cc8033635c395f41a66bcd
Omar-Ess <souafomar@gmail.com>
2021-09-30 15:04:15 +0100

    changed about section image and testimonials background image

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,7 +171,19 @@
     }
   });
 
+  /**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators");
+  let heroCarouselItems = select("#heroCarousel .carousel-item", true);
 
+  heroCarouselItems.forEach((item, index) => {
+    index === 0
+      ? (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>")
+      : (heroCarouselIndicators.innerHTML +=
+          "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>");
+  });
   /**
    * Menu carousel indicators
    */
